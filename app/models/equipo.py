516288c2f92472e588dad3fa3b0078a0f5b6499e
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-06-24 23:27:04 +0200

    Update equipo.py

--- app/models/equipo.py
+++ app/models/equipo.py
@@ -6,19 +6,37 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Enum as SQLEnum
 from app.database import Base
 
+
 class TipoEquipo(str, Enum):
-    COLO = "COLO"
-    CE = "CE"
+    ADP = "ADP"
+    ATS = "ATS"
+    LBP = "LBP"
+    MSB = "MSB"
+    NLP = "NLP"
+    PNL = "PNL"
     PDU = "PDU"
+
+
+class SubEquipo(str, Enum):
     BSW = "BSW"
+    FCB = "FCB"
+
 
 class Equipo(Base):
     __tablename__ = "equipos"
 
     id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String(100), nullable=False)
+    proyecto_id = Column(Integer, ForeignKey("proyectos.id"), nullable=False)
+
+    ubicacion_1 = Column(String(50), nullable=True)
+    ubicacion_2 = Column(String(50), nullable=True)
     tipo = Column(SQLEnum(TipoEquipo, name="tipo_equipo"), nullable=False)
-    proyecto_id = Column(Integer, ForeignKey("proyectos.id"))
+    sub_equipo = Column(SQLEnum(SubEquipo, name="sub_equipo"), nullable=True)
+
+    terminal = Column(String(50), nullable=True)
+    tipo_alimentacion = Column(String(50), nullable=True)
+    cable_set = Column(Integer, nullable=True)
+
     fecha_creacion = Column(DateTime, default=datetime.utcnow)
 
     # Relación con proyecto
