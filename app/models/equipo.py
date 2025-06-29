4a4370812a9ea5b6944dcdb6704339beb46b9043
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-06-29 23:18:04 +0200

    Update equipo.py

--- app/models/equipo.py
+++ app/models/equipo.py
@@ -39,4 +39,6 @@ class Equipo(Base):
     fecha_creacion = Column(DateTime, default=datetime.utcnow)
 
     proyecto = relationship("Proyecto", back_populates="equipos")
+    tests_continuidad = relationship("TestContinuidad", back_populates="equipo")
+tests_megado = relationship("TestMegado", back_populates="equipo")
 
