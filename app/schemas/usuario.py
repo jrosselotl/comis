e47488f89c74c7be5b7e073614afdf79313f990e
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-07 00:28:08 +0200

    Update usuario.py

--- app/schemas/usuario.py
+++ app/schemas/usuario.py
@@ -6,14 +6,13 @@ from datetime import datetime
 class RolUsuario(str, Enum):
     admin = "admin"
     tecnico = "tecnico"
+    proyecto = "proyecto" 
 
-# Para crear nuevos usuarios
 class UsuarioCreate(BaseModel):
     nombre: str
     correo: EmailStr
-    password: str  # solo en creación
+    password: str
 
-# Para mostrar datos del usuario
 class UsuarioOut(BaseModel):
     id: int
     nombre: str
@@ -21,9 +20,8 @@ class UsuarioOut(BaseModel):
     rol: RolUsuario
     fecha_registro: Optional[datetime]
 
-    model_config = ConfigDict(from_attributes=True)  # Pydantic v2 ✅
+    model_config = ConfigDict(from_attributes=True)
 
-# Para login
 class UsuarioLogin(BaseModel):
     correo: EmailStr
     password: str
