8c5b5dc4f678b1f94372434b21d49709c87d1692
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-07 08:16:59 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import pathlib
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
