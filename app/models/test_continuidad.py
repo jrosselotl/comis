568714952e7c6aba50314b803cc56b75a455a088
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-10 01:46:48 +0200

    Update test_continuidad.py

--- app/models/test_continuidad.py
+++ app/models/test_continuidad.py
@@ -22,7 +22,6 @@ class ResultadoContinuidad(Base):
     __tablename__ = "resultado_continuidad"
 
     id = Column(Integer, primary_key=True, index=True)
-    test_id = Column(Integer, ForeignKey("test_continuidad.id"), nullable=False)
     cable_set = Column(Integer, nullable=False)
     punto_prueba = Column(String(100), nullable=False)
     referencia_valor = Column(Float, nullable=True)
