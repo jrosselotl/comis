897b7ab02d05580d3385500ffd8390f99e75180e
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-10 01:19:17 +0200

    Update test_continuidad.py

--- app/models/test_continuidad.py
+++ app/models/test_continuidad.py
@@ -7,7 +7,7 @@ class TestContinuidad(Base):
     __tablename__ = "test_continuidad"
 
     id = Column(Integer, primary_key=True, index=True)
-    proyecto_id = Column(Integer, nullable=False)
+    proyecto_id = Column(Integer, ForeignKey("proyectos.id"), nullable=False)
     equipo_id = Column(Integer, ForeignKey("equipos.id"), nullable=False)
     usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=True)
     fecha = Column(DateTime, default=datetime.utcnow)
