94a029e8dc634efa13d702b03aeeac866e50efe2
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-12 23:19:46 +0200

    Update formulario_continuidad.js

--- app/static/js/formulario_continuidad.js
+++ app/static/js/formulario_continuidad.js
@@ -69,7 +69,7 @@ function initFormularioContinuidad(tipoAlimentacion) {
                         textoAdjunto.textContent = "📎 Archivo adjunto";
 
                         const reader = new FileReader();
-                        reader.onload = async () => {
+                        /*reader.onload = async () => {
                             try {
                                 const { data: { text } } = await Tesseract.recognize(reader.result, 'eng');
                                 const match = text.match(/[\d]+(?:[\.,]\d+)?\s?(?:kV|KV|Ω|ohm|MΩ|GΩ|V|mA|A)?/);
@@ -82,7 +82,7 @@ function initFormularioContinuidad(tipoAlimentacion) {
                                 console.error("Error OCR:", err);
                             }
                         };
-                        reader.readAsDataURL(file);
+                        reader.readAsDataURL(file);*/
                     } else {
                         textoAdjunto.textContent = "";
                     }
