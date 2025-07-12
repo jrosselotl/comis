56bb002d937eacf0668a0ef646fd10a896e5771f
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-13 01:36:23 +0200

    Update proyectos.py

--- app/routes/proyectos.py
+++ app/routes/proyectos.py
@@ -18,6 +18,16 @@ def crear_proyecto(proyecto: ProyectoCreate, db: Session = Depends(get_db)):
     db.refresh(nuevo)
     return nuevo
 
+@router.get("/proyectos/{proyecto_id}/tests")
+def obtener_tests_proyecto(proyecto_id: int, db: Session = Depends(get_db)):
+    relaciones = db.query(ProyectoTipoTest).filter_by(proyecto_id=proyecto_id).all()
+    lista_tests = []
+    for rel in relaciones:
+        test = db.query(Test).filter_by(id=rel.tipo_test).first()
+        if test:
+            lista_tests.append({"id": test.id, "nombre": test.nombre})
+    return lista_tests
+
 # Obtener todos los proyectos
 @router.get("/", response_model=list[ProyectoOut])
 def listar_proyectos(db: Session = Depends(get_db)):
