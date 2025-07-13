3b97dfe4956355b5b220bad8ae0817f2519bfefe
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-13 23:36:04 +0200

    Update formulario_continuidad.js

--- app/static/js/formulario_continuidad.js
+++ app/static/js/formulario_continuidad.js
@@ -175,7 +175,8 @@ function initFormularioContinuidad(tipoAlimentacion) {
         formData.append("numero_tipo_equipo", numero_tipo_equipo);
         formData.append("sub_equipo", sub_equipo);
         formData.append("numero_sub_equipo", numero_sub_equipo);
-        formData.append("tipo_prueba", "continuidad");
+        const test_id = document.getElementById("tipo-prueba").selectedOptions[0]?.getAttribute("data-id");
+        formData.append("test_id", test_id);
         formData.append("cable_sets", cableSets);
         formData.append("tipo_alimentacion", tipo_alimentacion);
         formData.append("terminal", terminal);
