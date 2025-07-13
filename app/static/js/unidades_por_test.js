fe7971df243abb73a1ca97d79c3ded2eb8d31395
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-13 11:57:22 +0200

    Create unidades_por_test.js

---
+++ app/static/js/unidades_por_test.js
@@ -0,0 +1,12 @@
+// static/js/unidades_por_test.js
+
+window.UNIDADES_POR_TEST = {
+    continuidad: ["Ω", "mΩ", "kΩ", "MΩ"],
+    megado: ["MΩ", "GΩ", "kΩ"],
+    contact_resistance: ["µΩ", "mΩ", "Ω"],
+    torque: ["Nm", "Nmm", "kNm"],
+    voltaje: ["V", "mV", "kV"],
+    corriente: ["A", "mA", "kA", "µA"],
+    tiempo: ["ms", "s", "min", "h"]
+    // futuro: presion: ["bar", "psi", "kPa"]
+};
