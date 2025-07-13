d2b425a3ced1ce20ed2d9e125b3322a26fc5e1d7
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-13 11:57:53 +0200

    Update unidades_por_test.js

--- app/static/js/unidades_por_test.js
+++ app/static/js/unidades_por_test.js
@@ -1,12 +1,21 @@
 // static/js/unidades_por_test.js
 
-window.UNIDADES_POR_TEST = {
-    continuidad: ["Ω", "mΩ", "kΩ", "MΩ"],
-    megado: ["MΩ", "GΩ", "kΩ"],
-    contact_resistance: ["µΩ", "mΩ", "Ω"],
-    torque: ["Nm", "Nmm", "kNm"],
-    voltaje: ["V", "mV", "kV"],
-    corriente: ["A", "mA", "kA", "µA"],
-    tiempo: ["ms", "s", "min", "h"]
-    // futuro: presion: ["bar", "psi", "kPa"]
-};
+(function () {
+    const UNIDADES_POR_TEST = {
+        continuidad: ["Ω", "mΩ", "kΩ", "MΩ"],
+        megado: ["MΩ", "GΩ", "kΩ"],
+        contact_resistance: ["µΩ", "mΩ", "Ω"],
+        torque: ["Nm", "Nmm", "kNm"],
+        voltaje: ["V", "mV", "kV"],
+        corriente: ["A", "mA", "kA", "µA"],
+        tiempo: ["ms", "s", "min", "h"]
+        // Futuro: presion: ["bar", "psi", "kPa"]
+    };
+
+    // Validación simple por si ya fue definida
+    if (!window.UNIDADES_POR_TEST) {
+        window.UNIDADES_POR_TEST = UNIDADES_POR_TEST;
+    } else {
+        console.warn("UNIDADES_POR_TEST ya estaba definida en el contexto global.");
+    }
+})();
