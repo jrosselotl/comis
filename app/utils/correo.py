b3ccded3f9ea4a9e06b79e6af367e4933ec35668
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-14 21:52:01 +0200

    Update correo.py

--- app/utils/correo.py
+++ app/utils/correo.py
@@ -3,6 +3,9 @@ import smtplib
 from email.message import EmailMessage
 import os
 from dotenv import load_dotenv
+from sqlalchemy.orm import Session
+from app.models.usuario import Usuario
+from app.models.usuarios_proyectos import UsuarioProyecto
 
 load_dotenv()
 
@@ -25,3 +28,13 @@ def enviar_correo_con_pdf(destinatarios: list[str], asunto: str, cuerpo: str, ar
     with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as smtp:
         smtp.login(SMTP_USER, SMTP_PASS)
         smtp.send_message(msg)
+def obtener_correos_admins(db: Session, proyecto_id: int) -> list[str]:
+    usuarios_admins = (
+        db.query(Usuario)
+        .join(UsuarioProyecto, Usuario.id == UsuarioProyecto.usuario_id)
+        .filter(UsuarioProyecto.proyecto_id == proyecto_id)
+        .filter(Usuario.rol.in_(["admin", "proyecto"]))
+        .all()
+    )
+    correos = [u.correo for u in usuarios_admins]
+    return correos or ["soporte@miempresa.com"]  # Fallback
