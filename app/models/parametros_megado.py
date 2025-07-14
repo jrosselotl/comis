7c92429d434c0d9665ffbbd11a25a6e8ac677d22
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-14 23:32:25 +0200

    Update parametros_megado.py

--- app/models/parametros_megado.py
+++ app/models/parametros_megado.py
@@ -9,7 +9,6 @@ class ParametroMegado(Base):
     tipo_equipo = Column(String, nullable=False)
     unidad = Column(String, nullable=False)
     voltaje = Column(String, nullable=True)     # visible solo para referencia
-    corriente = Column(String, nullable=True)   # también como referencia
     duracion = Column(String, nullable=True)    # visible, no editable
     logica = Column(String, nullable=False)     # Ej: 'mayor_igual'
     referencia = Column(String, nullable=False)
