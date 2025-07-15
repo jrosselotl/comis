e6802e038a0efdb03ba7633da053b6bba4270372
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-15 14:35:01 +0200

    Update usuarios_proyectos.py

--- app/models/usuarios_proyectos.py
+++ app/models/usuarios_proyectos.py
@@ -7,9 +7,8 @@ from app.database import Base
 class UsuarioProyecto(Base):
     __tablename__ = "usuarios_proyectos"
 
-    id = Column(Integer, primary_key=True, index=True)
-    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
-    proyecto_id = Column(Integer, ForeignKey("proyectos.id"))
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"), primary_key=True)
+    proyecto_id = Column(Integer, ForeignKey("proyectos.id"), primary_key=True)
 
     usuario = relationship("Usuario", back_populates="proyectos_asociados")
     proyecto = relationship("Proyecto", back_populates="usuarios_asociados")
