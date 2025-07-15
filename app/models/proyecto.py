5b40e19655531a467439b58c53600c682ae1be8e
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-15 19:05:04 +0200

    Update proyecto.py

--- app/models/proyecto.py
+++ app/models/proyecto.py
@@ -8,18 +8,20 @@ class Proyecto(Base):
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
 
-    # Relación con equipos
     equipos = relationship("Equipo", back_populates="proyecto")
-
-    # Relación con usuarios asignados a proyectos
     usuarios_asociados = relationship("UsuarioProyecto", back_populates="proyecto")
 
-    # Logos
     logo_cliente = Column(String, nullable=True)
     logo_subcontrata = Column(String, nullable=True)
 
-    # ✅ Relaciones con tests (coinciden con los modelos de test)
+    # Tests
     tests_continuidad = relationship("TestContinuidad", back_populates="proyecto")
     tests_megado = relationship("TestMegado", back_populates="proyecto")
     tests_contact_resistance = relationship("TestContactResistance", back_populates="proyecto")
     tests_torque = relationship("TestTorque", back_populates="proyecto")
+
+    # Parámetros
+    parametros_continuidad = relationship("ParametroContinuidad", back_populates="proyecto")
+    parametros_megado = relationship("ParametroMegado", back_populates="proyecto")
+    parametros_contact_resistance = relationship("ParametroContactResistance", back_populates="proyecto")
+    parametros_torque = relationship("ParametroTorque", back_populates="proyecto")
