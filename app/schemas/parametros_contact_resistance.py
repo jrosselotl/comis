38366967c47f02759cb9a4765c32218c4e0ff68c
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-15 08:21:19 +0200

    Update parametros_contact_resistance.py

--- app/schemas/parametros_contact_resistance.py
+++ app/schemas/parametros_contact_resistance.py
@@ -2,15 +2,20 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ParametroContactResistanceBase(BaseModel):
+    proyecto_id: int
+    test_id: int
+    codigo_equipo: str
     logica: str
     referencia: str
     unidad: str
-    proyecto_id: int
 
 class ParametroContactResistanceCreate(ParametroContactResistanceBase):
     pass
 
 class ParametroContactResistanceUpdate(BaseModel):
+    proyecto_id: Optional[int]
+    test_id: Optional[int]
+    codigo_equipo: Optional[str]
     logica: Optional[str]
     referencia: Optional[str]
     unidad: Optional[str]
