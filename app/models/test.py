079b16d686009e9f37b2d5385ba9fee581eb2f64
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-15 11:44:48 +0200

    Update test.py

--- app/models/test.py
+++ app/models/test.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 class Test(Base):
@@ -7,3 +8,8 @@ class Test(Base):
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String(100), nullable=False, unique=True)
 
+    # Clave foránea para vincular con Equipo
+    equipo_id = Column(Integer, ForeignKey("equipos.id"), nullable=False)
+
+    # Relación inversa
+    equipo = relationship("Equipo", back_populates="tests")
