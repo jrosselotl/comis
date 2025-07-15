3abf36f2bc4dda7c8e6874dc7ab1fba97e840671
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-15 16:02:18 +0200

    Update proyecto.py

--- app/models/proyecto.py
+++ app/models/proyecto.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Stringhttps://github.com/jrosselotl/comis/tree/main
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.database import Base
 
