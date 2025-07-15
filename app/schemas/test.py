72abb9f90fa5aa39d3cb9851226f191e67174b66
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-15 10:40:49 +0200

    Update test.py

--- app/schemas/test.py
+++ app/schemas/test.py
@@ -1,19 +1,13 @@
 from pydantic import BaseModel
-from datetime import datetime
-from typing import Optional
 
 class TestBase(BaseModel):
-    tipo_prueba: str
-    equipo_id: int
+    nombre: str
 
 class TestCreate(TestBase):
-    """Esquema para crear un Test"""
     pass
 
 class TestOut(TestBase):
-    """Esquema para devolver un Test en respuestas"""
     id: int
-    fecha: datetime
 
     class Config:
         orm_mode = True
