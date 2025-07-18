cde50b5c5b7f6c8ca52209565806450cee5b2978
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-18 23:46:11 +0200

    Update tipo_equipo.py

--- app/models/tipo_equipo.py
+++ app/models/tipo_equipo.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, String, ARRAY
+from sqlalchemy import Column, Integer, String, JSON
 from app.database import Base
 
 class TipoEquipo(Base):
     __tablename__ = "tipo_equipos"
 
     id = Column(Integer, primary_key=True, index=True)
-    tipo_equipo = Column(String, nullable=False)
-    numero_tipo_equipo = Column(ARRAY(Integer), default=[])
-    sub_equipo = Column(String, nullable=True)
-    numero_sub_equipo = Column(ARRAY(Integer), default=[])
+    tipo_equipo = Column(String(50), nullable=False)
+    numero_tipo_equipo = Column(JSON, nullable=True)  # Ej: [1,2,3,4]
+    sub_equipo = Column(String(50), nullable=True)
+    numero_sub_equipo = Column(JSON, nullable=True)   # Ej: [1,2,3]
