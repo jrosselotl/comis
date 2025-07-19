71d5d6b271eafefa10af2601b1d8726578c2a504
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-19 14:56:38 +0200

    Update test_torque.py

--- app/models/test_torque.py
+++ app/models/test_torque.py
@@ -1,35 +1,34 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from app.database import Base
 
 class TestTorque(Base):
-    __tablename__ = "tests_torque"
+    __tablename__ = "test_torque"
 
     id = Column(Integer, primary_key=True, index=True)
     test_id = Column(Integer, ForeignKey("test.id"), nullable=False)
     project_id = Column(Integer, ForeignKey("project.id"))
     equipment_id = Column(Integer, ForeignKey("equipment.id"))
-    tipo_alimentacion = Column(String, nullable=True)
+    power_type = Column(String, nullable=True)
     user_id = Column(Integer, ForeignKey("user.id"))
 
-    resultados = relationship("ResultadoTorque", back_populates="test")
-    project = relationship("Project", back_populates="tests_torque")
-    equipment = relationship("Equipment", back_populates="tests_torque")
-    user = relationship("User", back_populates="tests_torque")
+    result = relationship("ResultTorque", back_populates="test")
+    project = relationship("Project", back_populates="test_torque")
+    equipment = relationship("Equipment", back_populates="test_torque")
+    user = relationship("User", back_populates="test_torque")
 
 
-class ResultadoTorque(Base):
-    __tablename__ = "resultados_torque"
+class ResultTorque(Base):
+    __tablename__ = "result_torque"
 
     id = Column(Integer, primary_key=True, index=True)
-    test_id = Column(Integer, ForeignKey("tests_torque.id"), nullable=False)
+    test_id = Column(Integer, ForeignKey("test_torque.id"), nullable=False)
     cable_set = Column(Integer, nullable=False)
-    punto_prueba = Column(String, nullable=False)
-    valor_nominal = Column(Float, nullable=False)  # se copia del parametro_torque
-    valor_comprobacion = Column(Float, nullable=True)  # técnico lo ingresa
-    unidad = Column(String, nullable=False)
-    observaciones = Column(String, nullable=True)
-    imagen = Column(String, nullable=True)
+    test_point = Column(String, nullable=False)
+    nominal_value = Column(Float, nullable=False)      # copied from torque parameter
+    verification_value = Column(Float, nullable=True)  # entered by technician
+    unit = Column(String, nullable=False)
+    observation = Column(String, nullable=True)
+    image_url = Column(String, nullable=True)
 
-    test = relationship("TestTorque", back_populates="resultados")
-
+    test = relationship("TestTorque", back_populates="result")
