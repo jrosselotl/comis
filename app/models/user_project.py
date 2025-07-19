aa5ea6f30f481542f6cedd6d51464067b4902f5f
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-19 21:36:38 +0200

    Update and rename project_user.py to user_project.py

--- app/models/user_project.py
+++ app/models/user_project.py
@@ -1,3 +1,4 @@
+# app/models/user_project.py
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
@@ -5,8 +6,10 @@ from app.database import Base
 class UserProject(Base):
     __tablename__ = "user_project"
 
-    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    project_id = Column(Integer, ForeignKey("project.id"), primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    project_id = Column(Integer, ForeignKey("project.id"), nullable=False)
 
     user = relationship("User", back_populates="project_user")
     project = relationship("Project", back_populates="user_project")
+
