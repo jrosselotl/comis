7f30f05acb6937759e440429286e3945060fbe0b
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-19 09:13:17 +0200

    Update usuario.py

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -10,7 +10,7 @@ class RolUsuario(str, enum.Enum):
     proyecto = "proyecto"
 
 class Usuario(Base):
-    __tablename__ = "usuarios"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, nullable=False)
@@ -19,9 +19,9 @@ class Usuario(Base):
     rol = Column(String, default="tecnico")  # Puede ser admin, tecnico o proyecto
     fecha_registro = Column(DateTime, default=datetime.utcnow)
 
-    tests_continuidad = relationship("TestContinuidad", back_populates="usuario")
-    tests_megado = relationship("TestMegado", back_populates="usuario")
-    tests_contact_resistance = relationship("TestContactResistance", back_populates="usuario")
-    tests_torque = relationship("TestTorque", back_populates="usuario")
+    tests_continuidad = relationship("TestContinuidad", back_populates="user")
+    tests_megado = relationship("TestMegado", back_populates="user")
+    tests_contact_resistance = relationship("TestContactResistance", back_populates="user")
+    tests_torque = relationship("TestTorque", back_populates="user")
 
-    proyectos_asociados = relationship("UsuarioProyecto", back_populates="usuario")
+    proyectos_asociados = relationship("UsuarioProyecto", back_populates="user")
