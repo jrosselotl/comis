23ae348cfa5e94781e1096d7e0b9234e85229ccc
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-19 14:29:40 +0200

    Update location.py

--- app/models/location.py
+++ app/models/location.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, JSON
 from app.database import Base
 
-class Ubicacion(Base):
+
+class Location(Base):
     __tablename__ = "location"
 
     id = Column(Integer, primary_key=True, index=True)
     project_id = Column(Integer, ForeignKey("project.id"), nullable=False)
 
-    ubicacion_1 = Column(String(50), nullable=False)
-    numero_ubicacion_1 = Column(JSON, nullable=True)  # Ej: [1,2,3]
-    ubicacion_2 = Column(String(50), nullable=True)
-    numero_ubicacion_2 = Column(JSON, nullable=True)  # Ej: [1,2,3]
+    location_1 = Column(String(50), nullable=False)
+    location_1_number = Column(JSON, nullable=True)  # Example: [1,2,3]
+    location_2 = Column(String(50), nullable=True)
+    location_2_number = Column(JSON, nullable=True)  # Example: [1,2,3]
