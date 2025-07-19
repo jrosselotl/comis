cd68e05d935c9dac99067455c579db77a78efb4a
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-19 15:01:41 +0200

    Update project_user.py

--- app/models/project_user.py
+++ app/models/project_user.py
@@ -1,14 +1,12 @@
-# app/models/usuarios_proyectos.py
-
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
-class UsuarioProyecto(Base):
-    __tablename__ = "usuarios_proyectos"
+class UserProject(Base):
+    __tablename__ = "user_project"
 
     user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
     project_id = Column(Integer, ForeignKey("project.id"), primary_key=True)
 
-    user = relationship("User", back_populates="proyectos_asociados")
-    project = relationship("Project", back_populates="usuarios_asociados")
+    user = relationship("User", back_populates="project_user")
+    project = relationship("Project", back_populates="user_project")
