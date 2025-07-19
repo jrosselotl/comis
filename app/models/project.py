81edec136d7e7affd94facd1099daa53abe27ada
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-19 14:30:21 +0200

    Update project.py

--- app/models/project.py
+++ app/models/project.py
@@ -2,21 +2,21 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.database import Base
 
-class Proyecto(Base):
+
+class Project(Base):
     __tablename__ = "project"
 
     id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, unique=True, nullable=False)
+    name = Column(String, unique=True, nullable=False)
 
     equipment = relationship("Equipment", back_populates="project")
-    usuarios_asociados = relationship("UsuarioProyecto", back_populates="project")
+    users_associated = relationship("UserProject", back_populates="project")
 
-    logo_cliente = Column(String, nullable=True)
-    logo_subcontrata = Column(String, nullable=True)
+    client_logo = Column(String, nullable=True)
+    subcontractor_logo = Column(String, nullable=True)
 
     # Tests
-    tests_continuidad = relationship("TestContinuidad", back_populates="project")
-    tests_megado = relationship("TestMegado", back_populates="project")
-    tests_contact_resistance = relationship("TestContactResistance", back_populates="project")
-    tests_torque = relationship("TestTorque", back_populates="project")
-
+    test_continuity = relationship("TestContinuity", back_populates="project")
+    test_isolation = relationship("TestIsolation", back_populates="project")
+    test_contact_resistance = relationship("TestContactResistance", back_populates="project")
+    test_torque = relationship("TestTorque", back_populates="project")
