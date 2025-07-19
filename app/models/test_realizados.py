db3e50fbc075a14a8e0adef9993dd9847aeb510a
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-19 09:10:41 +0200

    Update test_realizados.py

--- app/models/test_realizados.py
+++ app/models/test_realizados.py
@@ -7,13 +7,13 @@ class TestRealizado(Base):
     __tablename__ = "test_realizados"
 
     id = Column(Integer, primary_key=True, index=True)
-    proyecto_id = Column(Integer, ForeignKey("proyectos.id", ondelete="CASCADE"))
-    equipo_id = Column(Integer, ForeignKey("equipos.id", ondelete="CASCADE"))
-    usuario_id = Column(Integer, ForeignKey("usuarios.id", ondelete="CASCADE"))
-    test_id = Column(Integer, ForeignKey("tests.id", ondelete="CASCADE"))
-    fecha = Column(DateTime, default=datetime.utcnow)
+    project_id = Column(Integer, ForeignKey("project.id", ondelete="CASCADE"))
+    equipment_id = Column(Integer, ForeignKey("equipment.id", ondelete="CASCADE"))
+    user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
+    test_id = Column(Integer, ForeignKey("test.id", ondelete="CASCADE"))
+    date = Column(DateTime, default=datetime.utcnow)
     estado = Column(String(50), default="Incompleto")
 
-    proyecto = relationship("Proyecto")
-    equipo = relationship("Equipo")
-    usuario = relationship("Usuario")
+    projet = relationship("Project")
+    equipment = relationship("Equipment")
+    user = relationship("User")
