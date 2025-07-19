5f4ce7e07e669727862861f94811177b54ec8006
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-19 08:45:36 +0200

    Update project.py

--- app/models/project.py
+++ app/models/project.py
@@ -3,20 +3,20 @@ from sqlalchemy.orm import relationship
 from app.database import Base
 
 class Proyecto(Base):
-    __tablename__ = "proyectos"
+    __tablename__ = "project"
 
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
 
-    equipos = relationship("Equipo", back_populates="proyecto")
-    usuarios_asociados = relationship("UsuarioProyecto", back_populates="proyecto")
+    equipos = relationship("Equipo", back_populates="project")
+    usuarios_asociados = relationship("UsuarioProyecto", back_populates="project")
 
     logo_cliente = Column(String, nullable=True)
     logo_subcontrata = Column(String, nullable=True)
 
     # Tests
-    tests_continuidad = relationship("TestContinuidad", back_populates="proyecto")
-    tests_megado = relationship("TestMegado", back_populates="proyecto")
-    tests_contact_resistance = relationship("TestContactResistance", back_populates="proyecto")
-    tests_torque = relationship("TestTorque", back_populates="proyecto")
+    tests_continuidad = relationship("TestContinuidad", back_populates="project")
+    tests_megado = relationship("TestMegado", back_populates="project")
+    tests_contact_resistance = relationship("TestContactResistance", back_populates="project")
+    tests_torque = relationship("TestTorque", back_populates="project")
 
