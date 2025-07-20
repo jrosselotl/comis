a85e39f4cc3ef557398506c511bd71dc3b0e6caf
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-20 05:19:12 +0200

    Update user_project.py

--- app/models/user_project.py
+++ app/models/user_project.py
@@ -1,4 +1,4 @@
-_project# app/models/user_project.py
+# app/models/user_project.py
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
