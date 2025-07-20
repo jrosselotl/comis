852099a648b6610493bd15ba60626e76448bb296
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-21 00:37:51 +0200

    Update location.py

--- app/models/location.py
+++ app/models/location.py
@@ -9,6 +9,6 @@ class Location(Base):
     project_id = Column(Integer, ForeignKey("project.id"), nullable=False)
 
     location_1 = Column(String(50), nullable=False)
-    location_1_number = Column(JSON, nullable=True)  # Example: [1,2,3]
+    number_location_1 = Column(JSON, nullable=True)  # Example: [1,2,3]
     location_2 = Column(String(50), nullable=True)
-    location_2_number = Column(JSON, nullable=True)  # Example: [1,2,3]
+    number_location_2 = Column(JSON, nullable=True)  # Example: [1,2,3]
