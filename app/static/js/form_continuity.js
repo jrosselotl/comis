a3925d7f8bf4de4692ee5ad712364232e8c8ee23
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-21 20:54:39 +0200

    Update form_continuity.js

--- app/static/js/form_continuity.js
+++ app/static/js/form_continuity.js
@@ -68,6 +68,11 @@ function initFormContinuity(powerType) {
                             <input type="text" name="${idResult}" id="${idResult}" />
                         </div>
                     </td>
+                    <td>
+                      <select name="unit_${i}_${point}">
+                        <option value="${selectedUnit}">${selectedUnit}</option>
+                      </select>
+                    </td>
                     <td><input name="observation_${i}_${point}" type="text" /></td>
                     <td>
                         <label class="camera-label">
