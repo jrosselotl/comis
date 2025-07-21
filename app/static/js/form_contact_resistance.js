ffb0523854205cd72eb138033e470d52fbc9fe57
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-21 23:31:37 +0200

    Update form_contact_resistance.js

--- app/static/js/form_contact_resistance.js
+++ app/static/js/form_contact_resistance.js
@@ -70,7 +70,7 @@ function initFormContactResistance(powerType) {
                         </div>
                     </td>
                     <td>
-                    <input type="text" name="unit_${i}_${point}" value="${selectedUnit}" readonly />
+                    <input type="text" value="${selectedUnit}" readonly name="unit_${i}_${point}" />
                     </td>
 
                     <td><input name="observation_${i}_${point}" type="text" /></td>
@@ -169,7 +169,7 @@ function initFormContactResistance(powerType) {
         formData.append("data", JSON.stringify(data));
         images.forEach(img => formData.append("images", img));
 
-        const response = await fetch("/form/contact_resistance/save", {
+        const response = await fetch("/form/save", {
             method: "POST",
             body: formData
         });
