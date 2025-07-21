5930f039dcf585586c272da5f861be19f0f949e4
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-21 20:53:22 +0200

    Update form_contact_resistance.js

--- app/static/js/form_contact_resistance.js
+++ app/static/js/form_contact_resistance.js
@@ -50,6 +50,7 @@ function initFormContactResistance(powerType) {
                 <tr>
                     <th>Point</th>
                     <th>Result / N/A</th>
+                    <th>Unit</th>
                     <th>Observation</th>
                     <th>Image</th>
                 </tr>`;
