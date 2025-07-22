9ccf10334194697013ef28a2cfce0fb67c649166
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-22 06:34:43 +0200

    Update form_continuity.js

--- app/static/js/form_continuity.js
+++ app/static/js/form_continuity.js
@@ -143,7 +143,7 @@ function initFormContinuity(powerType) {
                     cable_set: i,
                     test_point: point,
                     result_value: result,
-                    unit: selectedUnit,
+                    unit: document.getElementById("unit")?.value || "",
                     observation: observation
                 });
 
