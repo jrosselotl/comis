4c7e54b900a5eda5798d61e17f7e1741dc552c2e
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-22 23:22:10 +0200

    Update form.py

--- app/routes/form.py
+++ app/routes/form.py
@@ -49,22 +49,7 @@ async def save_form(
     images: list[UploadFile] = File(...),
     db: Session = Depends(get_db)
 ):
-    # ✅ DEBUG: imprime lo recibido para verificar que no venga None o mal tipado
-    print("📥 Recibido:", {
-        "project_id": project_id,
-        "location_1": location_1,
-        "number_location_1": number_location_1,
-        "location_2": location_2,
-        "number_location_2": number_location_2,
-        "equipment_type": equipment_type,
-        "number_equipment_type": number_equipment_type,
-        "sub_equipment": sub_equipment,
-        "number_sub_equipment": number_sub_equipment,
-        "test_type": test_type,
-        "cable_set": cable_set,
-        "power_type": power_type,
-        "terminal": terminal
-    })
+
     data_parsed = json.loads(data)
     user_id = 1  # ✅ Will be dynamic (authenticated user)
 
