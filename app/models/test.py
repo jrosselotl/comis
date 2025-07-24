fe963393d33ad8cacf101cbc7418d539c655f3f9
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-25 00:40:22 +0200

    Update test.py

--- app/models/test.py
+++ app/models/test.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 class Test(Base):
@@ -7,3 +8,7 @@ class Test(Base):
     id = Column(Integer, primary_key=True, index=True)
     test_type = Column(String(100), unique=True, nullable=False)
     description = Column(Text, nullable=True)
+
+    # ✅ Relaciones correctas
+    test_project = relationship("TestProject", back_populates="test")
+    test_performed = relationship("TestPerformed", back_populates="test")
