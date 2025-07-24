b96ffb40e1da1209bee2d39e8aee679667f35dc7
jrosselotl <71625480+jrosselotl@users.noreply.github.com>
2025-07-24 06:26:02 +0200

    Update result_continuity.py

--- app/models/result_continuity.py
+++ app/models/result_continuity.py
@@ -1,1 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class ResultContinuity(Base):
+    __tablename__ = "result_continuity"
+
+    id = Column(Integer, primary_key=True, index=True)
+    test_performed_id = Column(Integer, ForeignKey("test_performed.id"), nullable=False)
+
+    test_point = Column(String(50), nullable=False)
+    result_value = Column(Float)
+    cable_set = Column(Integer)
+    power_type = Column(String(50))
+    unit = Column(String(20))
+    image_url = Column(String(255))
+    observation = Column(String)
+
+    test_performed = relationship("TestPerformed")
 
